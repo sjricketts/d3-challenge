e7d13a1e3311634050b3a8e44e886a11fea7ce89
Scott Ricketts <SJR@imac.lan>
2020-09-27 16:28:36 -0500

    Read in data; will not log in console

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,22 @@ var svg = d3
 var chartGroup = svg
   .append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+
+// Read In Data from CSV
+
+// Initial Params - includes any axis selection that has multiple options
+// var chosenXAxis = "poverty";
+
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("./data.csv").then(function(demoData, err) {
+  if (err) throw err;
+   
+  // parse data - set values to numerical data types
+  demoData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+    
+   console.log(demoData);
+  });
